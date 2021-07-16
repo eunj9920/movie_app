c89cefa1ff726db2571a76ab3301a26d13e2daf9
unknown <eunj9920@naver.com>
2021-07-16 22:40:33 +0900

    third commit

--- src/App.js
+++ src/App.js
@@ -1,60 +1,22 @@
 import React from "react";
+import { HashRouter,Route } from "react-router-dom"
+import Home from "./routes/Home"
+import About from "./routes/About"
+import Navigation from "./components/Navigation"
+import Detail from "./routes/Detail"
+import "./App.css"
 
-
-
-const foodILike = [
-  {
-    id:1,
-    name: "Kimchi",
-    image:
-      "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg"
-  },
-  {
-    id:2,
-    name: "Samgyeopsal",
-    image:
-      "https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg"
-  },
-  {
-    id:3,
-    name: "Bibimbap",
-    image:
-      "http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb"
-  },
-  {
-    id:4,
-    name: "Doncasu",
-    image:
-      "https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg"
-  },
-  {
-    id:5,
-    name: "Kimbap",
-    image:
-      "http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg"
-  }
-];
-
-
-function Food( {name,picture} ){
-  return(
-    <div>
-      <h2>I like {name}</h2>
-      <img src={picture} alt={name}></img>
-    </div>
-    
-  );
-}
-
-function App() {
+function App(){
   return (
-    <div>
-      {foodILike.map(dish => (
-      <Food key={dish.id} name={dish.name} picture={dish.image} ></Food>
-      ))}
-    </div>
-    
-  );
+  <HashRouter>
+    <Navigation></Navigation>
+    <Route path="/" exact={true} component={Home}>
+    </Route>
+    <Route path="/about" component={About}>
+    </Route>
+    <Route path="/movie/:id" component={Detail}></Route>
+  </HashRouter>
+  )
 }
 
-export default App;
+export default App
